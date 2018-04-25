d4f6c0c676a91d983152365f2a71ad98669a5ff5
Lishuai <li.shuai@cgtn.com>
2018-04-25 16:45:24 +0800

    jsckson

--- src/main/java/cn/edu/ruc/info/jackson/Province.java
+++ src/main/java/cn/edu/ruc/info/jackson/Province.java
@@ -6,7 +6,32 @@ package cn.edu.ruc.info.jackson;
  */
 
 public class Province {
+
     public String name;
     public int population;
     public String[] city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String[] getCity() {
+        return city;
+    }
+
+    public void setCity(String[] city) {
+        this.city = city;
+    }
 }
