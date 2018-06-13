b78f0d60e7ea54279b76899d4f2eb62a89b88721
Shuai <li.shuai@cgtn.com>
2018-06-13 15:03:06 +0800

    country list --> java.util.Locale

---
+++ src/main/java/cn/edu/ruc/info/CountryList.java
@@ -0,0 +1,35 @@
+package cn.edu.ruc.info;
+
+import java.util.Locale;
+
+/**
+ * @Author: Lishuai
+ * @Date: Created on 2018/6/13
+ */
+public class CountryList {
+
+    public static void main(String[] args) {
+        CountryList obj = new CountryList();
+        obj.run();
+        //get a list of countries and display in FRENCH
+        obj.getListOfCountries(Locale.FRENCH);
+    }
+
+    public void run() {
+        String[] locales = Locale.getISOCountries();
+        for (String countryCode : locales) {
+            Locale obj = new Locale("", countryCode);
+            System.out.println("Country Code = " + obj.getCountry() + ", Country Name = " + obj.getDisplayCountry());
+        }
+        System.out.println("Done");
+    }
+
+    public void getListOfCountries(Locale locale) {
+        String[] locales = Locale.getISOCountries();
+        for (String countryCode : locales) {
+            Locale obj = new Locale("", countryCode);
+            System.out.println("Country Code = " + obj.getCountry() + ", Country Name = " + obj.getDisplayCountry(locale));
+        }
+    }
+}
+
