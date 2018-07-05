8627c2aea470f5f07afb0c226b41e442bd6313a2
Shuai <li.shuai@cgtn.com>
2018-07-05 14:25:14 +0800

    country list --> java.util.Locale

---
+++ src/main/java/cn/edu/ruc/info/csv/Developer.java
@@ -0,0 +1,47 @@
+package cn.edu.ruc.info.csv;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: Lishuai
+ * @Date: Created on 2018/6/22
+ */
+public class Developer {
+
+    private String name;
+    private BigDecimal salary;
+    private int age;
+
+    public Developer(String name, BigDecimal salary, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    //...
+}
