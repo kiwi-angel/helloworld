c1e7cade4947ccb8976ef0875f03db47dd2e3683
Shuai <li.shuai@cgtn.com>
2018-09-18 18:30:22 +0800

    design pattenStrategy

---
+++ src/main/java/cn/edu/ruc/info/strategy/CashSuper.java
@@ -0,0 +1,9 @@
+package cn.edu.ruc.info.strategy;
+
+/**
+ * @Author: Lishuai
+ * @Date: Created on 2018/9/18
+ */
+public interface CashSuper {
+    double acceptCash(double money);
+}
---
+++ src/test/java/cn/edu/ruc/info/strategy/CashClientTest.java
@@ -0,0 +1,22 @@
+package cn.edu.ruc.info.strategy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @Author: Lishuai
+ * @Date: Created on 2018/9/18
+ */
+public class CashClientTest {
+
+    @Test
+    public void cashTest() {
+        double total = 0;
+        CashContext cashContext = new CashContext("return");
+        total = cashContext.getResult(2000);
+        assertEquals(total, 1400);
+    }
+
+
+}
